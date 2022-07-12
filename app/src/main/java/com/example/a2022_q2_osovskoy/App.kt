8e9440e1e01a489f4e1f78e69ccdf2e6a0379a14
Осовской Даниил <neurocap@gmail.com>
2022-07-12 16:24:05 +0300

    App refactoring

--- app/src/main/java/com/example/a2022_q2_osovskoy/App.kt
+++ app/src/main/java/com/example/a2022_q2_osovskoy/App.kt
@@ -1,10 +1,6 @@
 package com.example.a2022_q2_osovskoy
 
 import android.app.Application
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
 import com.example.a2022_q2_osovskoy.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -16,33 +12,10 @@ class App : Application(), HasAndroidInjector {
     @Inject
     lateinit var injector: DispatchingAndroidInjector<Any>
 
-    companion object {
-        const val CHANNEL_NAME = "my_channel"
-        const val CHANNEL_DESCRIPTION = "push Timer time"
-        const val CHANNEL_ID = "123456"
-    }
-
     override fun onCreate() {
         super.onCreate()
         DaggerAppComponent.factory().create(this).inject(this)
-
-        createNotificationChannel()
     }
 
     override fun androidInjector(): AndroidInjector<Any> = injector
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = CHANNEL_NAME
-            val descriptionText = CHANNEL_DESCRIPTION
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-
-            val notificationManager: NotificationManager =
-                this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
 }
